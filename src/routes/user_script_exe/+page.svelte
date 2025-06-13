13c0ccfe52f8332d29d16bd76380bb80cf5b8d58
uguisu64 <soma64soma@gmail.com>
2025-06-14 02:12:57 +0900

    Add button to list installed packages

--- src/routes/user_script_exe/+page.svelte
+++ src/routes/user_script_exe/+page.svelte
@@ -1,15 +1,18 @@
 <script>
     import { loadPyodideInstance } from "$lib/loadPyodide";
+    import { text } from "@sveltejs/kit";
     import { onMount } from "svelte";
 
     let errList = $state([])
     let outList = $state([])
     let editordiv
     let pyEditor
+    let moduleName = $state()
 
     onMount(async() => {
         const { editor, KeyCode, KeyMod } = await import('monaco-editor/esm/vs/editor/editor.main.js')
         pyodide = await loadPyodideInstance()
+        await pyodide.loadPackage("micropip")
         pyodide.setStdout({batched: (str) => outList.push(str)})
         pyodide.setStderr({batched: (str) => outList.push(str)})
         pyEditor = editor.create(editordiv, {
@@ -21,19 +24,60 @@
         pyEditor.addCommand(KeyMod.CtrlCmd | KeyCode.Enter, exeScript)
     })
 
+    let installed_packages;
+
     async function exeScript() {
         const userPyScript = pyEditor.getValue()
         pyodide = await loadPyodideInstance()
         try {
-            pyodide.runPython(userPyScript)
+            if (userPyScript === "") {
+                const micropip = pyodide.pyimport("micropip")
+                installed_packages = micropip.list()
+                console.log(micropip)
+                let output = ""
+                for (const [package_name, package_version] of Object.entries(installed_packages.toJs())) {
+                    output += package_name + "==" + package_version + "\\n"
+                }
+                outList.push(output)
+            } else {
+                pyodide.runPython(userPyScript)
+            }
         }
         catch(e) {
             errList.push(e)
         }
     }
+
+    async function get_installed_packages() {
+        pyodide = await loadPyodideInstance()
+        try {
+            const installed_package = pyodide.runPython(`
+import micropip
+
+installed_package = micropip.list()
+
+print(installed_package)
+`)
+            outList.push(installed_package)
+        } catch (e) {
+            errList.push(e)
+        }
+    }
+
+    async function pipinstall(pythonModule) {
+        pyodide = await loadPyodideInstance()
+        const micropip = pyodide.pyimport("micropip")
+        return await micropip.install(pythonModule)
+    }
 </script>
 
 <h1>ユーザスクリプト実行</h1>
+
+<input type="text" bind:value={moduleName}>
+
+<button onclick={() => (pipinstall(moduleName))}>pip install</button>
+
+<button onclick={get_installed_packages}>List installed packages</button>
 
 <h2>Pythonスクリプトエディタ</h2>
 
